fd8f8ca618f7cf0135ddf989ce6093040652d024
namnd <namnguyennd7428@gmail.com>
2025-07-13 21:07:21 +0700

    date time field database

--- src/main/java/com/spring/core/entity/Country.java
+++ src/main/java/com/spring/core/entity/Country.java
@@ -1,11 +1,19 @@
 package com.spring.core.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @Table(name = "country")
@@ -17,6 +25,17 @@ public class Country {
 
     @Column(name = "country_name")
     private String countryName;
+
+//    @Column(name = "created_at")
+//    @CreationTimestamp
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdAt;
+//
+//    @Column(name = "updated_at")
+//    @UpdateTimestamp
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime updatedAt;
 
     public Country() {
 
